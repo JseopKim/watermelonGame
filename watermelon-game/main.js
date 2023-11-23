4bd73511cba9b656c63401ac650fc09609c0f081
JseopKim <jsub5646@gmail.com>
2023-11-24 00:32:11 +0900

    재생과 정지 영역 만들어서 bgm 껐다켜기까지 완성

--- watermelon-game/main.js
+++ watermelon-game/main.js
@@ -14,7 +14,7 @@ const render = Render.create({
   element: document.body,
   options: {
     wireframes: false, //게임 형태이기 때문에 false로 설정
-    background: "#F7F4C8",
+    background: "#F9CBBE",
     width: 620,
     height: 850,
   }
@@ -24,24 +24,24 @@ const world = engine.world;
 
 const leftWall = Bodies.rectangle(15, 395, 30, 790, {
   isStatic: true,
-  render: {fillStyle: "#E6B143"},
+  render: {fillStyle: "#EE6D49"},
 })
 
 const rightWall = Bodies.rectangle(605, 395, 30, 790, {
   isStatic: true,
-  render: {fillStyle: "#E6B143"},
+  render: {fillStyle: "#EE6D49"},
 })
 
 const ground = Bodies.rectangle(310, 820, 620, 60, {
   isStatic: true,
-  render: {fillStyle: "#E6B143"},
+  render: {fillStyle: "#EE6D49"},
 })
 
 const topLine = Bodies.rectangle(310, 150, 620, 2, {
   name: "topLine",
   isStatic: true,
   isSensor: true, //감지만 한다.
-  render: {fillStyle: "#E6B143"},
+  render: {fillStyle: "#EE6D49"},
 })
 
 World.add(world, [leftWall, rightWall, ground, topLine]);
@@ -57,20 +57,50 @@ let disableAction = false; //1초동안 동작이 되지 않도록
 let interval = null;
 let winCount = 0;
 let playBox = document.createElement("div");
-let gameBox = document.getElementById("app");
-gameBox.appendChild(playBox);
+let play = document.createElement("div");
+let stop = document.createElement("div");
+
+document.body.style.margin = 0;
+document.body.style.display = "flex";
+document.body.style.flexDirection = "row";
+
+document.body.appendChild(playBox);
 
 playBox.style.width = "100px";
 playBox.style.height = "40px";
-playBox.style.backgroundColor = "#888888";
-playBox.style.position = "fixed";
-playBox.style.left = "530px";
+playBox.style.backgroundColor = "#D73E14";
+playBox.style.display = "flex";
+
+//playBox안에 재생과 일시정지 넣기
+playBox.appendChild(play);
+playBox.appendChild(stop);
+
+play.style.width = "50%";
+play.style.height = "100%";
+play.style.display = "flex";
+play.style.justifyContent = "center";
+play.style.alignItems = "center";
+play.innerText = "재생";
+
+stop.style.width = "50%";
+stop.style.height = "100%";
+stop.style.display = "flex";
+stop.style.justifyContent = "center";
+stop.style.alignItems = "center";
+stop.innerText = "정지"
+
+
 
 function playAudio() {
   audioContext.resume().then(() => {
     audioElement.play();
-    // 이벤트 리스너를 한 번만 실행하도록 제거
-    window.removeEventListener("click", playAudio);
+    console.dir(audioElement);
+  });
+}
+
+function stopAudio() {
+  audioContext.resume().then(() => {
+    audioElement.pause();
   });
 }
 
@@ -93,7 +123,28 @@ function addFruit() {
   World.add(world, body);
 }
 
-window.addEventListener("click", playAudio);
+play.addEventListener("click", playAudio);
+play.addEventListener("mouseenter", function() {
+  // 마우스가 요소 안으로 진입할 때의 동작
+  this.style.cursor = "pointer"; // 여기서 this는 이벤트가 발생한 요소를 가리킵니다.
+});
+
+// mouseleave 이벤트에 대한 리스너 추가 (마우스가 요소를 벗어날 때의 동작)
+play.addEventListener("mouseleave", function() {
+  // 커서 모양 원래대로 변경
+  this.style.cursor = "auto";
+});
+stop.addEventListener("click", stopAudio);
+stop.addEventListener("mouseenter", function() {
+  // 마우스가 요소 안으로 진입할 때의 동작
+  this.style.cursor = "pointer"; // 여기서 this는 이벤트가 발생한 요소를 가리킵니다.
+});
+
+// mouseleave 이벤트에 대한 리스너 추가 (마우스가 요소를 벗어날 때의 동작)
+stop.addEventListener("mouseleave", function() {
+  // 커서 모양 원래대로 변경
+  this.style.cursor = "auto";
+});
 
 window.onkeydown = (event) => {
   if (disableAction) {
